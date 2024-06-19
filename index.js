3ebbda6617281e5b84c955278b336e5e64847086
blaze <prabhatkumarverma42@gmail.com>
2024-06-19 16:20:00 +0530

    index.js: entry point file, added path redirection route and funcnality to it

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const { connectMongoDb } = require("./connection.js");
+const urlRouter = require("./routes/urls.js");
+const { URL } = require("./models/urls.js");
+
+connectMongoDb("mongodb://127.0.0.1:27017/url-compression");
+
+const app = express();
+const PORT = 8000;
+
+// Middlewares
+app.use(express.json());
+
+//Routes
+app.use("/api/url", urlRouter);
+app.route("/:shortUrl").get(async (req, res) => {
+  const url = req.params.shortUrl;
+  if (!url) {
+    return res.status(400).json({
+      status: "failed",
+      msg: "url is required",
+    });
+  }
+  try {
+    const originalUrl = await URL.findOneAndUpdate(
+      {
+        shortUrl: url,
+      },
+      {
+        $push: {
+          visitHistory: {
+            timestamp: Date.now(),
+          },
+        },
+      },
+    );
+    if (!originalUrl) {
+      return res.status(400).json({
+        status: "failed",
+        msg: "invalid url",
+      });
+    }
+    console.log("Success");
+    console.log(originalUrl);
+    res.redirect(originalUrl.url);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      status: failed,
+      msg: "something went wrong",
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("Server Started");
+});
